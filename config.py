4f96cabbbe0fbfe6bf9f1771f3de6b7389bcf8eb
Your Name <you@example.com>
2025-05-28 23:38:22 +0500

    admins fix

--- config.py
+++ config.py
@@ -14,12 +14,12 @@ START_TIME = time.time()
 
 API_KEY = os.environ.get('API_ID') or os.getenv('API_ID') or "API_KEY"
 API_HASH = os.environ.get('API_HASH') or os.getenv("API_HASH")   or "API_HASH"
-BOT_TOKEN = os.environ.get('BOT_TOKEN ') or os.getenv("BOT_TOKEN") or   "BOT_TOKEN"
-ADMINS=[]
-ADMINS = os.environ.get('ADMIN_IDS') or os.getenv("ADMIN_IDS") or "123456789"
-ADMINS = ADMINS.split(",") # Split the string and convert it to list
-OWNER_ID = os.environ.get('OWNER_ID') or os.getenv("OWNER_ID") or "123456789" # Owner ID
-ADMINS+=[OWNER_ID] # Add the owner ID to the list
+BOT_TOKEN = os.environ.get('BOT_TOKEN') or os.getenv("BOT_TOKEN") or   "BOT_TOKEN"
+ADMINS = os.environ.get('ADMINS') or os.getenv('ADMINS') or os.environ.get('ADMIN_IDS') or os.getenv('ADMIN_IDS') or "123456789"
+ADMINS = [x.strip() for x in ADMINS.split(",") if x.strip()]
+OWNER_ID = os.environ.get('OWNER_ID') or os.getenv('OWNER_ID') or "123456789" # Owner ID
+if str(OWNER_ID) not in ADMINS:
+    ADMINS.append(str(OWNER_ID))
 LOG_CHANNEL = os.environ.get('LOG_CHANNEL') or os.getenv("LOG_CHANNEL") or "advchatgptlogs" # Log Channel username preferance
 DATABASE_URL=os.environ.get('DATABASE_URL') or os.getenv("DATABASE_URL") or "DATABASE_URL"
 BING_COOKIE = os.environ.get('BING_COOKIE') or os.getenv("BING_COOKIE") or "BING_COOKIE"
@@ -28,14 +28,8 @@ OCR_KEY = os.environ.get('OCR_KEY') or os.getenv("OCR_KEY") or "OCR_KEY"
 
 #check if all ids are int or not
 for x in ADMINS:
-    x = str(x)
-    if not x.isdigit():
+    if not str(x).isdigit():
         sys.exit("Please enter a valid integer ID value, Check your ADMINS list")
-    else:
-        pass
-
-if not OWNER_ID.isdigit():  
-    sys.exit("Please enter a valid integer ID value, Check your OWNER_ID")
 
 ADMINS = list(map(int, ADMINS))
 OWNER_ID = int(OWNER_ID)
